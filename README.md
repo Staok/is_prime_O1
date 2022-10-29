af16ae28f8330803fdbd2ebb20b9141a0ba2182c
Staok <superxhy@qq.com>
2022-10-29 14:15:16 +0800

    .

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # is_prime_O1
 生成一个判断一个数是否为质数的算法时间复杂度为O(1)的程序的程序（整活儿）
+
+下载仓库：`clone git@github.com:Staok/is_prime_O1.git`。
+
+win 平台上编译：
+
+1. 编译主程序：`gcc main.c -o main.exe`，运行完会生成一个 `is_prime.c` 文件；
+
+2. 再编译 `is_prime.c` 文件：`gcc is_prime.c -o is_prime.exe`，会得到 `is_prime.exe`；
+
+3. 运行 `is_prime.exe`，即可输入一个数字，程序会在 O(1) 时间复杂度下返回这个数是否质数~
+
+4. 在 main.c 文件中 修改 `HOW_MANY_DO_YOU_WANT` 宏 即可修改生成文件 `is_prime.c` 中判断质数的范围。
+
+   ```c
+   /* 请问您生成一个判断多少以内的质数的时间复杂度为O(1)的程序呢亲~
+       只要您的磁盘容得下呢~经验值，填入10w生成的文件约为10MB */
+   #define HOW_MANY_DO_YOU_WANT 1000
+   ```
+
+------
+
+起初，是无意间看到一个图：
+
+![判断质数整活](assets/判断质数整活.jpg)
+
+刚看了图的时候，以为是整个什么活笑死，后来猛然意识到这玩意儿写的这么规律...
+
+用程序生成这个程序好了~（说不定上图也是这么整出来的）
---
+++ main.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/* 请问您生成一个判断多少以内的质数的时间复杂度为O(1)的程序呢亲~
+    只要您的磁盘容得下呢~经验值，填入10w生成的文件约为10MB */
+#define HOW_MANY_DO_YOU_WANT 1000
+
+unsigned char is_prime(unsigned long long num);
+
+int main(void)
+{
+    char *message[2] = {"__________start!__________","__________end!__________"},quitkey;
+    printf("\n%s\n",message[0]);
+
+    /* 程序开始________________________________ */
+
+    FILE* outfile = fopen("is_prime.c","w");
+        if(!outfile) return -1;
+    
+    fputs(
+        "#include <stdio.h>\n"
+        "#include <stdlib.h>\n\n\n"
+        "int main(void)\n{\n"
+        "    unsigned long long num = 0;\n"
+        "    printf(\"input a num:\");\n"
+        "    scanf(\"\%llu\",&num);\n"
+        "    printf(\"\\n\");\n\n"
+        ,outfile
+    );
+
+    for(unsigned long long i = 1;i <= HOW_MANY_DO_YOU_WANT;i++)
+    {
+        fprintf(
+            outfile,
+            "    if(num == %llu)\n"
+            "    {\n"
+            ,i
+        );
+
+        if(is_prime(i))
+        {
+            fprintf(
+                outfile,
+                "        printf(\"%llu is a prime\\n\");\n"
+                ,i
+            );
+        }else
+        {
+            fprintf(
+                outfile,
+                "        printf(\"%llu is not a prime\\n\");\n"
+                ,i
+            );
+        }
+
+        fprintf(
+            outfile,
+            "    }\n\n"
+        );
+    }
+
+    fputs("    system(\"pause\");\n\n",outfile);
+    fputs("    return 0;\n\n",outfile);
+
+    fputs("}\n\n\n",outfile);
+
+    fclose(outfile);
+
+    /* ________________________________程序结束 */
+	printf("\n%s\n",message[1]);
+    printf("Press 'q' key to exit.");
+    // while('q' != quitkey){scanf("%c",&quitkey);}
+    while('q' != getchar());
+    return 0;
+}
+
+/* 判断质数 */
+unsigned char is_prime(unsigned long long num)
+{
+    unsigned long long n;
+    
+    if(num == 1) return 0;
+
+    n = (unsigned long long)sqrt((double)num);
+
+    for(unsigned long long i = 2;i <= n;i++)
+    {
+        if(num % i == 0)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
